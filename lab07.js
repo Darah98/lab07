1e320a65c1098c729771519e9d78f8c7ffb2e287
Darrrah <darashalabi@gmail.com>
2020-02-06 02:52:36 +0200

    lab07 created

--- lab07.js
+++ lab07.js
@@ -1,1 +1,18 @@
 
+function bmiCalculator() {
+     var x = prompt(“Input Weight in kilograms”);
+      var y = prompt(“Input Height in meters”); 
+parseInt(String: Number)
+var weight= parseInt(x);
+var height = parseInt(y);
+      var bmi = weight/height; 
+      if (bmi >= 25) { 
+          alert=(bmi , “Overweight”); 
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            alert=(bmi , "Normal"); 
+            else if (bmi <= 18.5) {
+                alert=(bmi , "Underweight"); 
+return bmi;
+            };
+        };
+    function bmiCalculator(;)
